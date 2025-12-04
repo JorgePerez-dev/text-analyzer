4f7c585b0fcbc9c5985d19ad7e69ea3e81698c07
Jorge Pérez Aranaz <jperezaranaz@gmail.com>
2025-12-04 15:46:23 +0100

    Initial commit - add sales commission project

---
+++ Día 2/proyecto.py
@@ -0,0 +1,12 @@
+print("Programa ventas")
+
+
+nombre = input("Cual es tu nombre: ")
+ventas = int(input("Cuanto vendiste: "))
+
+comisión = round(ventas * 13) / 100
+
+
+print(f"{nombre} tu comisión es {comisión} ")
+
+
